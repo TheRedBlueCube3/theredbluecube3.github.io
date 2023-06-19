0b7e057eed744f415c197716e4f8f492a415c0a8
TheRedBlueCube3 <98588842+TheRedBlueCube3@users.noreply.github.com>
2023-06-19 14:22:07 +0300

    add

---
+++ assets/mouse-follower/script.js
@@ -0,0 +1,7 @@
+var cursor = document.getElementById('cursor');
+document.addEventListener('mousemove', (e)=>{
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+})
